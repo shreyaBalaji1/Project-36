7834e1925d059931d5a64a8cea3a2d0cb0f7e9c3
Shreya <123@gmail.com>
2020-10-31 12:27:02 -0500

    Virtual Pet 1

--- sketch.js
+++ sketch.js
@@ -1,9 +1,55 @@
-function setup() {
-  createCanvas(800,400);
-  createSprite(400, 200, 50, 50);
+//Create variables here
+var dog, dogImg, happyDogImg, database, foodS, foodStock;
+
+function preload() {
+  //load images here
+  dogImg = loadImage("images/dogImg.png");
+  happyDogImg = loadImage("images/dogImg1.png");
 }
 
-function draw() {
-  background(255,255,255);  
-  drawSprites();
+function setup() {
+  createCanvas(500, 500);
+  
+  database = firebase.database();
+
+  dog = createSprite(250, 305, 10, 10);
+  dog.addImage(dogImg);
+  dog.scale = 0.18;
+
+  foodStock = database.ref('Food');
+  foodStock.on("value", readStock);
 }
+
+
+function draw() {  
+  background(46, 139, 87);
+
+  if(keyWentDown(UP_ARROW)) {
+    writeStock(foodS);
+    dog.addImage(happyDogImg);
+  }
+
+  drawSprites();
+  //add styles here
+  textSize(16);
+  fill("white");
+  text("NOTE:Press UP_ARROW key to feed Drago milk!", 75, 50);
+  text("Food remaining:" + foodS, 180, 390);
+}
+
+function readStock(data) {
+  foodS = data.val();
+}
+
+function writeStock(x) {
+  if(x<=0) {
+    x = 0;
+  }
+  else{
+    x = x-1;
+  }
+  database.ref('/').update({
+    Food: x
+  })
+}
+
